34167d84bd8355f67fd60d6ea69665c139ee3d62
soleimani1984 <z.soleimani1984@gmail.com>
2019-10-28 11:53:45 +0100

    Die klasse

--- src/AdventureDiceGame.java
+++ src/AdventureDiceGame.java
@@ -1,4 +1,34 @@
 public class AdventureDiceGame {
 
+        //Creat two Die objects and rolls them several times.
+        public static void main(String[] args) {
 
-}
+            Die die1, die2;
+
+            // Integers der styr på summen af terningerne der bliver slået
+            int sum;
+
+            die1 = new Die();
+            die2 = new Die();
+
+            die1.roll();
+            die2.roll();
+            System.out.println("Die one: " + die1 + ", Die Tow: " + die2);
+
+            die1.roll();
+            die2.setFaceValue(4);
+
+            System.out.println("Die One: " + die1 + ", Die Two: " + die2);
+
+            sum = die1.getFaceValue() + die2.getFaceValue();
+            System.out.println("Sum: " + sum);
+
+            sum = die1.roll() + die2.roll();
+            System.out.println("Die One: " +die1 + ", Die Tow: " + die2);
+            System.out.println("New sum: " + sum);
+
+        }
+    }
+
+
+
